b3b8b5b0eb4afa8b997b0c0a9b8c2468dc01e653
Alfredo Arrieta <alfredoarrieta99@gmail.com>
2024-01-29 12:05:09 -0300

    router

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
 );
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
